070f1eda6360ab8f67c3e84fc1f8d2cd72853f25
AlexCsanyi <alex.csanyi@gmail.com>
2019-06-10 06:08:37 +0100

    imported jquery and bootstrap in the App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import "./App.css";
+import "./index.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
 
-function App() {
-  return 
-  <div className="App">
-    
-  </div>;
+global.jQuery = require("jquery");
+require("bootstrap");
+
+class App extends React.Component {
+  render() {
+    return <div className="App">Hello Pomodoro Project</div>;
+  }
 }
 
 export default App;
+
+ReactDOM.render(<App />, document.getElementById("root"));
