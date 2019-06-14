0bd85edecf2727847b386eb226598bb8bc81b1fc
AlexCsanyi <alex.csanyi@gmail.com>
2019-06-14 07:03:39 +0100

    finished the basic app

--- src/Components/TimerDisplay.jsx
+++ src/Components/TimerDisplay.jsx
@@ -1,24 +1,36 @@
 import React from "react";
 import * as timerStates from "../timerStates";
+// import { Player, ControlBar } from "video-react";
+import "../../node_modules/video-react/dist/video-react.css";
 
 const leftPad = val => {
   if (val < 10) return `0${val}`;
   return `${val}`;
 };
 
-function TimerDisplay(props) {
-  return (
-    <div>
-      <div className="center-block embed-responsive">
-        {props.timerState === timerStates.COMPLETE}
+class TimerDisplay extends React.Component {
+  render() {
+    return (
+      <div>
+        {this.props.timerState === timerStates.COMPLETE && (
+          <iframe
+            title="myvideo"
+            src="https://player.vimeo.com/video/82845229?autoplay=1&title=0&byline=0&portrait=0&badge=0"
+            width="640"
+            height="360"
+            frameborder="0"
+            allow="autoplay; fullscreen"
+            allowfullscreen
+          />
+        )}
+        <div className="card-header">
+          {`${leftPad(this.props.currentTime.get("hours"))} : 
+          ${leftPad(this.props.currentTime.get("minutes"))} :
+          ${leftPad(this.props.currentTime.get("seconds"))}`}
+        </div>
       </div>
-      <div className="card-header">
-        {`${leftPad(props.currentTime.get("hours"))} : 
-      ${leftPad(props.currentTime.get("minutes"))} :
-      ${leftPad(props.currentTime.get("seconds"))}`}
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default TimerDisplay;
